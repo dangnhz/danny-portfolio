c23663977b8b3f47136f4b5eeee45a8ec597c12d
Danny Ngo <danny@digitalgarden.com.au>
2022-06-09 17:32:49 +1000

    updated contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -7,7 +7,7 @@
       <PageTitle title="Get In Touch" />
       <div class="contact-detail">
         <p>
-          I'm currently looking for any new opportunities, my inbox is always open.
+          My inbox is always open.
           <br />Whether you have a question or just want to say hi, I'll try my best to get back to you!
         </p>
         <a href="mailTo: dangnh3091@gmail.com">
